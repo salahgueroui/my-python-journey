b98d1c8c99f6e0b8b935ab18e328a3d99bcedb1e
Salah Gueroui <salaheddinegueroui50@gmail.com>
2025-11-30 23:21:53 +0100

    Add Challeng solution

---
+++ data-types-challenge/data-types-challenge.py
@@ -0,0 +1,11 @@
+age = 21
+height = 1.80
+name = "Salah"
+student = True
+fullname = ""
+
+print(f"value = {age} ------ type = {type(age)}")
+print(f"value = {height} ------ type = {type(height)}")
+print(f"value = {name} ------ type = {type(name)} ------ length = {len(name)}")
+print(f"value = {student} ------ type = {type(student)}")
+print(f"value = {fullname} ------ type = {type(fullname)} ------ length = {len(fullname)}")
