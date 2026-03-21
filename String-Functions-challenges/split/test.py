266542e11e8bec57d9702dd22d9b96fec855eb9c
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-03-21 06:48:26 +0100

    Add Challeng solution

---
+++ String-Functions-challenges/split/test.py
@@ -0,0 +1,10 @@
+#exemples
+#1
+stamp = "2026-03-21 10:40:21"
+print(stamp.split("-"))
+#2
+csv_line = "Maria,Data Engineer,27"
+
+print(csv_line.split(",")[2])
+print(csv_line.split(",")[1])
+print(csv_line.split(",")[0])
