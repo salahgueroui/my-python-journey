4b617da77b6270a7614f6ca34f9f66cff7fc0660
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-03-21 07:52:38 +0100

    Add Challeng solution

---
+++ 3-String-Functions-challenges/cleaning/Case Conversion/challenge.py
@@ -0,0 +1,22 @@
+#convert to lower case
+a="salah GUEROUI"
+print(a.lower())
+
+#convert to upper case
+b="salah gueroui"
+print(b.upper())
+
+#check if the string is equal to another string
+c="SALAH"
+d="salah"
+print(c==d)
+
+#check if the string is equal to another string before removing spaces and converting them to lower case
+c="SALAH".lower()
+d=" salah".lower()
+print(c==d)
+
+#check if the string is equal to another string after removing spaces and converting them to lower case
+c="SALAH".lower()
+d="salah".lower()
+print(c==d)
