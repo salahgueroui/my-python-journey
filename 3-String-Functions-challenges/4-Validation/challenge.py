3726cc12f50fbc59ec4a601d6d6c4be7d129a005
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-03-22 13:51:24 +0100

    Add Challeng solution

---
+++ 3-String-Functions-challenges/4-Validation/challenge.py
@@ -0,0 +1,9 @@
+#check if the string contains only letters
+country = "Switzerland"
+print(country.isalpha())
+
+#check if the string contains only numbers
+phone_number = "01761234567"
+print(phone_number.isnumeric())
+
+
