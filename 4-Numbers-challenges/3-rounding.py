352339d1facd4eae468c2e907fd3f2623bc12be6
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-03-24 17:36:29 +0100

    Add Challeng solution

--- 4-Numbers-challenges/3-rounding.py
+++ 4-Numbers-challenges/3-rounding.py
@@ -1,2 +1,26 @@
 # Challenge 3: Rounding
 # Practice rounding numbers using round(), math.floor(), math.ceil()
+
+# Measure distance
+print(abs(2-10))
+
+#Rounding Numbers
+
+#round() rounds to the nearest even number
+x=25.76157883
+print(round(x,3))
+
+print(round(2.5))
+print(round(3.5))
+
+import math as m
+
+#floor() rounds down to the nearest integer
+print(m.floor(2.5))
+print(m.floor(3.5))
+
+#ceil() rounds up to the nearest integer
+print(m.ceil(2.5))
+print(m.ceil(3.5))
+
+
