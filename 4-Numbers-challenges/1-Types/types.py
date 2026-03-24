abc200ac907e14ab08d3eaba7fcedf0a4aae2c51
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-03-24 17:03:49 +0100

    Add Challeng solution

---
+++ 4-Numbers-challenges/1-Types/types.py
@@ -0,0 +1,34 @@
+# Numbers Types
+x=10
+y=10.5
+z=2+3j
+
+# print numbers types
+# int
+print(type(x))
+# float
+print(type(y))
+# complex
+print(type(z))
+
+# String Type
+a="10"
+# print string type
+print(type(a))
+# print string
+print(a*2)
+
+# convert string to int
+a=int(a)
+print(type(a))
+print(a*2)
+
+b="10.5"
+# convert string to float
+print(float(b))
+
+# Convert two integers to a complex number
+c = 3  # real part
+d = 4  # imaginary part
+
+print(complex(c, d))  # Output: (3+4j)
