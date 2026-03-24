98feaa683b3ae77db08d9dde7441243cc663ad81
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-03-24 18:31:15 +0100

    Add Challeng solution

--- 4-Numbers-challenges/5-validation.py
+++ 4-Numbers-challenges/5-validation.py
@@ -1,2 +1,16 @@
 # Challenge 6: Validation
 # Practice validating and converting number inputs
+ 
+#is_integer()
+x=7.0
+print(x.is_integer()) 
+
+y=7.5
+print(y.is_integer())   
+
+#isinstance() 
+a=10
+print(isinstance(a, int))
+b=10.5
+print(isinstance(b, float))
+
