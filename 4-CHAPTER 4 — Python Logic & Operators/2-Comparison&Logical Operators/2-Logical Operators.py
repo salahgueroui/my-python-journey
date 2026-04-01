b6afaa1170817ba7d9ba9f6e93d25d5e8f860028
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-04-01 20:59:35 +0200

    add new commit

---
+++ 4-CHAPTER 4 — Python Logic & Operators/2-Comparison&Logical Operators/2-Logical Operators.py
@@ -0,0 +1,27 @@
+print(10 < 5 and 10 > 15)
+print(10 > 5 or 10 < 15)
+print(10 > 5 and 10 < 15)
+print(10 > 5 or 10 < 15)
+
+#or
+cpu_usage=70
+memory_usage=95
+print(cpu_usage>90 or memory_usage>90)
+
+#and
+email="[EMAIL_ADDRESS]"
+password="[PASSWORD]"
+print(email=="[EMAIL_ADDRESS]" and password=="[PASSWORD]")
+
+#not
+print(not(10>5))
+print(not(10<5))
+print(not True)
+print(not False)
+print(not not True)
+
+name=""
+print(name)
+print(not name)
+print(not 0)
+
