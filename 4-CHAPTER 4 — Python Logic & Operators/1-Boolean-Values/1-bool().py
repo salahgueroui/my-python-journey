cf333ed53582a3d665e33747638adb6605f96c6a
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-04-01 18:22:53 +0200

    add new commit

---
+++ 4-CHAPTER 4 — Python Logic & Operators/1-Boolean-Values/1-bool().py
@@ -0,0 +1,17 @@
+# Boolean Values
+print(True)
+print(False)
+
+# Boolean Type
+print(type(True))
+print(type(False))
+
+# Boolean if empty 
+print(bool(123))
+print(bool("hi"))
+print(bool())
+print(bool(0))
+print(bool(""))
+print(bool(None))
+print(bool("A"))
+
---
+++ 4-CHAPTER 4 — Python Logic & Operators/1-Boolean-Values/2-all()&any().py
@@ -0,0 +1,7 @@
+email=""
+phone=""
+username="Salah"
+#any() returns True if at least one of the elements is True
+print(any([email, phone, username]))
+#all() returns True if all the elements are True
+print(all([email, phone, username]))
