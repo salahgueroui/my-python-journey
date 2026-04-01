24319f885f226495d05c69bb03a4569f70e4b1ed
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-04-01 18:56:56 +0200

    add new commit

---
+++ 4-CHAPTER 4 — Python Logic & Operators/2-Comparison&Logical Operators/Comparison & Logical Operators.py
@@ -0,0 +1,13 @@
+print(10 > 5)
+print(10 < 5)
+print(10 == 5)
+print(10 != 5)
+print(10 >= 5)
+print(10 <= 5)
+print('salah' == 'salah')
+print("A" == "a")
+print(2<3<4)
+print(2<4<1)
+
+age=20
+print(age>=18 and age<=60)
